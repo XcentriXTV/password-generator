f173cbc562b0f301f04f0536ae725af09b874703
Atla <30910804+XcentriXTV@users.noreply.github.com>
2025-01-01 21:05:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 length = input('Password length?')
 length = int(length)
 
-for p in range(10):
+for p in range(20): # Amend value produced
     password = ''
     for c in range(length):
         password += random.choice(chars)
